ec288c3aa89d81c6083159694135897e4c2d8770
roaa <roaa.almasoud@medicus.ai>
2021-12-27 08:39:33 +0200

    commit

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -29,18 +29,18 @@ export default class App extends Component {
                     <MainLayout>
                         <Switch>
 
-                            <Route exact path={'/demo1'} render={(props) => <FirstDemoComponent {...props}/>}/>
+                            <Route exact path={process.env.PUBLIC_URL + '/demo1'} render={(props) => <FirstDemoComponent {...props}/>}/>
 
-                            <Route exact path={'/'}
+                            <Route exact path={process.env.PUBLIC_URL + '/'}
                                    render={(props) => <FirstDemoComponent {...props}/>}/>
 
-                            <Route exact path={'/demo2'}
+                            <Route exact path={process.env.PUBLIC_URL + '/demo2'}
                                    render={(props) => <SecondDemo {...props}/>}/>
 
-                            <Route exact path={'/demo3'}
+                            <Route exact path={process.env.PUBLIC_URL + '/demo3'}
                                    render={(props) => <ThirdDemo {...props}/>}/>
 
-                            <Route exact path={'/demo4'}
+                            <Route exact path={process.env.PUBLIC_URL + '/demo4'}
                                    render={(props) => <FourthDemo {...props}/>}/>
 
                         </Switch>
